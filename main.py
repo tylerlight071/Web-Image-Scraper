cd6cd8cffbb39bbece421ea4d59fd50b791047fe
Tyler Lightwood <tylerlightwood071@gmail.com>
2024-05-14 19:46:48 +0100

    Refactor image crawler code and improve user interface

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import os
 import requests
+import time
 from bs4 import BeautifulSoup
+from colorama import Fore, Style, init
 
+# Initialize colorama
+init(autoreset=True)
 
 def title():
-    print("""
- ___  _____ ______   ________  ________  _______           ________  ________  ________  ___       __   ___       _______   ________     
-|\  \|\   _ \  _   \|\   __  \|\   ____\|\  ___ \         |\   ____\|\   __  \|\   __  \|\  \     |\  \|\  \     |\  ___ \ |\   __  \    
-\ \  \ \  \\\__\ \  \ \  \|\  \ \  \___|\ \   __/|        \ \  \___|\ \  \|\  \ \  \|\  \ \  \    \ \  \ \  \    \ \   __/|\ \  \|\  \   
- \ \  \ \  \\|__| \  \ \   __  \ \  \  __\ \  \_|/__       \ \  \    \ \   _  _\ \   __  \ \  \  __\ \  \ \  \    \ \  \_|/_\ \   _  _\  
-  \ \  \ \  \    \ \  \ \  \ \  \ \  \|\  \ \  \_|\ \       \ \  \____\ \  \\  \\ \  \ \  \ \  \|\__\_\  \ \  \____\ \  \_|\ \ \  \\  \| 
-   \ \__\ \__\    \ \__\ \__\ \__\ \_______\ \_______\       \ \_______\ \__\\ _\\ \__\ \__\ \____________\ \_______\ \_______\ \__\\ _\ 
-    \|__|\|__|     \|__|\|__|\|__|\|_______|\|_______|        \|_______|\|__|\|__|\|__|\|__|\|____________|\|_______|\|_______|\|__|\|__|
-                                                                                                                                         
-                                                                                                                                         
-                                                                                                                                         """)
+    print(Fore.GREEN + "\nImage Crawler" + Style.RESET_ALL)
 
-
+# Function to download image
 def download_image(url, filename):
     response = requests.get(url, stream=True)
     if response.status_code == 200:
@@ -24,7 +18,7 @@ def download_image(url, filename):
             for chunk in response.iter_content(1024):
                 file.write(chunk)
 
-
+# Function to search for images
 def crawl_images(url, save_dir):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -37,25 +31,40 @@ def crawl_images(url, save_dir):
         img_url = img.get('src')
         if not img_url.startswith('http'):
             img_url = url + img_url
+        print(Fore.BLUE + f"Downloading image {i+1} of {len(images)}..." + Style.RESET_ALL)
         download_image(img_url, f'{save_dir}/image{i}.jpg')
+        time.sleep(1)
 
+    print(Fore.GREEN + "Download complete!" + Style.RESET_ALL)
+    return len(images)
 
 # Print Title And Provide Choices
 title()
 
-print("\nDisclaimer: The author is not responsible for any misuse of this application.")
-print("Please respect copyright laws and terms of service of any website you use this on.")
+print(Fore.RED + "\nDisclaimer: The author is not responsible for any misuse of this application.")
+print("Please respect copyright laws and terms of service of any website you use this on." + Style.RESET_ALL)
 
+while True:
+    url = input(
+        Fore.YELLOW + "\nEnter a website URL to crawl for images (or 'q' to quit): " + Style.RESET_ALL)
+    if url.lower() == 'q':
+        break
 
-url = input("Enter a website URL to crawl for images: ")
+    print(Fore.YELLOW + "\n1. Download to the default Downloads folder" + Style.RESET_ALL) 
+    print(Fore.YELLOW + "2. Specify a different folder" + Style.RESET_ALL)
+    choice = input(Fore.YELLOW + "\nEnter your choice (1 or 2): " + Style.RESET_ALL)
 
-print("1. Download to the default Downloads folder")
-print("2. Specify a different folder")
-choice = input("Enter your choice (1 or 2): ")
+    if choice == '1':
+        save_dir = os.path.expanduser('~/Downloads')
+    else:
+        save_dir = input(
+            Fore.YELLOW + "\nEnter a directory to save the images: " + Style.RESET_ALL)
 
-if choice == '1':
-    save_dir = os.path.expanduser('~/Downloads')
-else:
-    save_dir = input("Enter a directory to save the images: ")
+    print(Fore.GREEN + "\nStarting image crawl..." + Style.RESET_ALL)
+    time.sleep(2)
 
-crawl_images(url, save_dir)
+    try:
+        num_images = crawl_images(url, save_dir)
+        print(Fore.GREEN + f"\nFound {num_images} images." + Style.RESET_ALL)
+    except Exception as e:
+        print(Fore.RED + f"\nAn error occurred: {e}" + Style.RESET_ALL)
